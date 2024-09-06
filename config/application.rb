eb9af354a1d5594fabd682ccbe346bfe17160791
mana <nouri393914@gmail.com>
2024-09-06 17:39:54 +0900

    static_page add

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # ファイル生成時の設定
+    config.generators do |g|
+      g.helper false
+      g.skip_routes true
+    end
   end
 end
