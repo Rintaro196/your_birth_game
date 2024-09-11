0a001f7107800b48b7891e25f841c2458c9c639a
mana <nouri393914@gmail.com>
2024-09-11 18:23:53 +0900

    lintエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "static_pages#home"
 
-  get 'users/new', to: 'users#new', as: 'new_users'
-  post 'users', to: 'users#submit', as: 'submit_users'
-  get 'users/result/:id', to: 'users#result', as: 'result'
+  get "users/new", to: "users#new", as: "new_users"
+  post "users", to: "users#submit", as: "submit_users"
+  get "users/result/:id", to: "users#result", as: "result"
 end
