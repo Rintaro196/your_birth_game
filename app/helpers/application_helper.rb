7c8749c87f5c0025e99ec7e267870a950c63dae1
mana <nouri393914@gmail.com>
2024-09-15 21:19:32 +0900

    ogp画像の設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Your Birth Game',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'あなたの誕生日に発売されたゲームは何かな？',
+      canonical: request.original_url,
+    },
+    og: {
+      sites_name: :site,
+      description: :description,
+      type: 'website',
+      url: request.original_url,
+      image: image_url('YourBirthGame.png'),
+      locale: 'ja_JP',
+    },
+    twitter: {
+      card: 'summary_large_image',
+    }
+  end
 end
