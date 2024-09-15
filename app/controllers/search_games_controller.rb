7cf032fa482fd14aff21cef31fc2574f9621fb40
mana <nouri393914@gmail.com>
2024-09-15 14:32:09 +0900

    日付形式を変更

--- app/controllers/search_games_controller.rb
+++ app/controllers/search_games_controller.rb
@@ -1,17 +1,18 @@
 class SearchGamesController < ApplicationController
   def search
     user = User.find(params[:user_id])
-    birth_date = user.birth_date
+    @birth_date = user.birth_date.strftime("%Y-%m-%d")
     
     response = HTTParty.get("https://api.rawg.io/api/games", {
       query: {
         key: ENV['RAWG_API_KEY'],
-        dates: "#{birth_date}",
+        dates: "#{@birth_date},#{@birth_date}",
         page_size: 1
-      }
+      },
+      headers: { 'Cache-Control' => 'no-cache' }
     })
 
-    @game = response.parsed_response["results"][0]
+    @game = response.parsed_response["results"]&.first
     render :result
   end
 end
