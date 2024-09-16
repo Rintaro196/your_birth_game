a94014f019119bbe06835950db5f4b992f52c505
mana <nouri393914@gmail.com>
2024-09-16 15:16:27 +0900

    twitter用ogpエラー解消

--- app/controllers/search_games_controller.rb
+++ app/controllers/search_games_controller.rb
@@ -2,14 +2,14 @@ class SearchGamesController < ApplicationController
   def search
     user = User.find(params[:user_id])
     @birth_date = user.birth_date.strftime("%Y-%m-%d")
-    
+
     response = HTTParty.get("https://api.rawg.io/api/games", {
       query: {
-        key: ENV['RAWG_API_KEY'],
+        key: ENV["RAWG_API_KEY"],
         dates: "#{@birth_date},#{@birth_date}",
         page_size: 1
       },
-      headers: { 'Cache-Control' => 'no-cache' }
+      headers: { "Cache-Control" => "no-cache" }
     })
 
     game_data = response.parsed_response["results"]&.first
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,26 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'Your Birth Game',
+      site: "Your Birth Game",
+      title: "Your Birth Game",
       reverse: true,
-      charset: 'utf-8',
-      description: 'あなたの誕生日に発売されたゲームは何かな？',
+      charset: "utf-8",
+      description: "あなたの誕生日に発売されたゲームは何かな？",
       canonical: request.original_url,
     og: {
-      site_name: :site,
-      description: :description,
-      type: 'website',
+      site_name: "Your Birth Game",
+      title: "Your Birth Game",
+      description: "あなたの誕生日に発売されたゲームは何かな？",
+      type: "website",
       url: request.original_url,
-      image: image_url('YourBirthGame.png'),
-      locale: 'ja_JP',
+      image: image_url("YourBirthGame.png"),
+      locale: "ja_JP"
     },
     twitter: {
-      card: 'summary_large_image',
-      image: image_url('YourBirthGame.png')
-    }}
+      card: "summary_large_image",
+      title: "Your Birth Game",
+      description: "あなたの誕生日に発売されたゲームは何かな？",
+      image: image_url("YourBirthGame.png")
+    } }
   end
 end
